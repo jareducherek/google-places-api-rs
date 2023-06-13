afdca5f7106f60d16b01897eb09177e26f6773c1
Collin Trovada <trovadacollin@gmail.com>
2023-06-13 17:31:50 -0500

    Think optional args are working in details example

--- examples/place_details.rs
+++ examples/place_details.rs
@@ -1,6 +1,7 @@
 use dotenv::dotenv;
 use std::env;
 use std::collections::HashSet;
+use isocountry::CountryCode;
 use google_places_api::client::GooglePlacesClient;
 use google_places_api::services::PlaceDetailsService;
 use google_places_api::models::constants::*;
@@ -24,19 +25,22 @@ async fn main() {
     let place_id = "ChIJN1t_tDeuEmsRUsoyG83frY4";
     
     // Define the fields as a HashSet
-    let fields: HashSet<PlaceDataField> = vec![
-        PlaceDataField::Name,
-        PlaceDataField::Rating,
-        PlaceDataField::PhoneNumber,
-    ]
+    //let fields: HashSet<PlaceDataField> = vec![
+    //    PlaceDataField::Name,
+    //    PlaceDataField::Rating,
+    //    PlaceDataField::PhoneNumber,
+    //].into_iter().collect();
 
-    let language: Language ;
-    .into_iter()
-    .collect();
+    let language: Language = Language::Es;
 
+    let region: CountryCode = CountryCode::USA;
+
+    let review_no_translation: bool = false;
+
+    let review_sort: ReviewSort = ReviewSort::Newest;
 
     // Perform the place details request
-    match place_details_service.get_place_details(place_id, Some(fields), None, None, None, None, None).await {
+    match place_details_service.get_place_details(place_id, None, Some(language), Some(region), Some(review_no_translation), Some(review_sort), None).await {
         Ok(place_details) => {
             // Display the place details
             println!("{}", place_details.display());
