ca6fe9175773882e521579b6853697a3440ca69c
Collin Trovada <trovadacollin@gmail.com>
2023-06-14 13:20:17 -0500

    New working example with optional args find_place

--- examples/find_place.rs
+++ examples/find_place.rs
@@ -1,9 +1,9 @@
 use dotenv::dotenv;
 use std::env;
+use std::collections::HashSet;
 use google_places_api::client::GooglePlacesClient;
 use google_places_api::services::PlaceSearchService;
 use google_places_api::models::constants::*;
-
 
 #[tokio::main]
 async fn main() {
@@ -21,17 +21,26 @@ async fn main() {
     let place_search_service = PlaceSearchService::new(client);
 
     // Define the search query, location, and radius
-    let input = "Mongolian Grill";
+    let input = "Museum of Contemporary Art Australia";
     let input_type = InputType::TextQuery;
+    let fields: HashSet<PlaceDataField> = vec![
+        PlaceDataField::Name,
+        PlaceDataField::Rating,
+        PlaceDataField::FormattedAddress,
+
+    ].into_iter().collect();
+    let language: Language = Language::En;
+    let location_bias: LocationBias = LocationBias::Circular {
+        radius: 10000,
+        latitude: 33.85984846198168,
+        longitude: 151.20907015422375,
+    };
 
     // Perform the place search
-    match place_search_service.find_place(input, input_type, None, None, None).await {
+    match place_search_service.find_place(input, input_type, Some(fields), Some(language), Some(location_bias)).await {
         Ok(find_place) => {
             // Process and display the search result
-            for result in find_place.results {
-                println!("Place:\n{}", result.display());
-            }
-            println!("Status: {}", find_place.status.as_str());
+            println!("{}", find_place.display());
         }
         Err(error) => {
             // Handle the error
