13d031056162eaedefeb3c85f01a25d7925d554f
Collin Trovada <trovadacollin@gmail.com>
2023-06-23 15:58:17 -0500

    Add optional params to text_search

--- src/services/place_search.rs
+++ src/services/place_search.rs
@@ -3,6 +3,7 @@ use crate::error::GooglePlacesError;
 use crate::models::place_search::{FindPlaceSearchResult, NearbySearchResult, TextSearchResult};
 use crate::models::constants::{PlaceDataField, Language, InputType, LocationBias,PlaceTypes, RankBy};
 use std::collections::HashSet;
+use isocountry::CountryCode;
 use urlencoding::encode;
 
 pub struct PlaceSearchService {
@@ -165,14 +166,51 @@ impl PlaceSearchService {
         &self,
         query: &str,
         radius: u32,
+        language: Option<Language>,
+        location: Option<(f64, f64)>,
+        max_price: Option<u32>,
+        min_price: Option<u32>,
+        open_now: Option<bool>,
+        page_token: Option<String>,
+        region: Option<CountryCode>,
+        place_type: Option<HashSet<PlaceTypes>>
     ) -> Result<TextSearchResult, GooglePlacesError>{
-        let url = format!(
+        let mut url = format!(
             "https://maps.googleapis.com/maps/api/place/textsearch/json?query={}&radius={}&key={}",
             query, radius, self.client.get_api_key()
         );
+        // Optional parameters
+        if let Some(language) = language {
+            url.push_str(&format!("&language={}", language.as_str()));
+        }
+        if let Some(location) = location {
+            url.push_str(&format!("&location={}%2C{}", location.0, location.1));
+        }
+        if let Some(max_price) = max_price {
+            url.push_str(&format!("&maxprice={}", max_price));
+        }
+        if let Some(min_price) = min_price {
+            url.push_str(&format!("&minprice={}", min_price));
+        }
+        if let Some(open_now) = open_now {
+            url.push_str(&format!("&opennow={}", open_now));
+        }
+        if let Some(page_token) = page_token {
+            url.push_str(&format!("&pagetoken={}", encode(&page_token)));
+        }
+        if let Some(region) = region {
+            url.push_str(&format!("&region={}", region.alpha2()));
+        }
+        if let Some(place_type) = place_type {
+            let type_list: Vec<String> = place_type.into_iter().map(|p| String::from(p.as_str())).collect();
+            let type_string = type_list.join(",");
+            url.push_str(&format!("&type={}", type_string));
+        }
+
         let response: reqwest::Response = self.client.get_req_client().get(&url).send().await.unwrap();
         let body: String = response.text().await.unwrap();
-        let search_result: TextSearchResult = serde_json::from_str(&body).unwrap();
+        let mut search_result: TextSearchResult = serde_json::from_str(&body).unwrap();
+        search_result.calculate_total_results();
         Ok(search_result)
     }
 }
